efcb96fd42c1acc636d36f3b71b193de45068f2b
Kenny Katzgrau <katzgrau@gmail.com>
2011-02-05 22:36:27 -0500

    Added analytics

--- application/views/global/_footer.php
+++ application/views/global/_footer.php
@@ -6,6 +6,18 @@
 
     <p>For <a href="http://codeigniter.com">CodeIgniter</a> &nbsp;&middot;&nbsp; Built by <a href="http://oconf.org">OConf</a></p>
 </div>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21216385-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
