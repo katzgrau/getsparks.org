f0e125c834ae73c69611765ff7ef6dec8c93e251
Kenny Katzgrau <katzgrau@gmail.com>
2011-02-13 17:10:23 -0500

    Changed button title

--- application/config/application.php
+++ application/config/application.php
@@ -4,7 +4,7 @@ $config['install_prototype_win'] = 'php tools/spark install';
 
 $config['install_prototype_nix'] = 'tools/spark install';
 
-$config['sparks_download_text'] = 'Get sparks-manager.zip';
+$config['sparks_download_text'] = 'Get Sparks';
 
 $config['sparks_download_url'] = 'http://getsparks.org/static/spark-manager.zip';
 
