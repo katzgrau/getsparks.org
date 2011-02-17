2e63894f9be9900369a9b862dd92e4c480979166
Kenny Katzgrau <katzgrau@gmail.com>
2011-02-16 23:45:12 -0500

    Added packages api controller .. left out somehow

---
+++ application/controllers/api/packages.php
@@ -0,0 +1,53 @@
+<?php
+
+class Packages extends CI_Controller
+{
+    /**
+     * The API to search for a package
+     * @param string $term The term to search for in a package
+     * @param string $format The return format
+     * @return $format
+     */
+    public function search()
+    {
+        $term = $this->input->get('q');
+        $this->load->model('spark');
+
+        if(strlen($term) <= 2)
+        {
+            $this->output->set_output(json_encode(array('success' => FALSE, 'message' => 'You must search with at least 2 characters')));
+            return;
+        }
+
+        $this->output->set_output(json_encode(array('success' => true, 'results' => Spark::search($term))));
+    }
+
+    /**
+     * The API call to get a spec
+     * @param string $package_name
+     * @param string $version
+     * @param string $format
+     */
+    public function spec($package_name, $version)
+    {
+        $this->load->model('spark');
+        $this->load->model('contributor');
+        $this->load->model('download');
+
+        $spark = Spark::get($package_name, $version);
+
+        if(!$spark) show_404 ();
+
+        $spark->spark_home  = base_url() . 'packages/' . $spark->name . '/show';
+        $spark->contributor = $spark->getContributor();
+
+        # Omit the password hash
+        unset($spark->contributor->password);
+
+        # Do this?
+        $spark->recordInstall();
+        Download::recordDownload('spec');
+
+        $this->output->set_output(json_encode(array('success' => true, 'spec' => json_encode($spark))));
+    }
+}
