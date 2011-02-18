0e7e3ce4c76023622ce5a3ae6d022d709d6ac38a
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-18 16:04:05 -0700

    Messing with the layout some more for interior pages

--- application/views/home/beau.php
+++ application/views/home/beau.php
@@ -1,12 +1,38 @@
 <?php $this->load->view('global/_new_header.php'); ?>
 
-<h2>Page Title</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
-<h3>Page Sub-Title</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+<div class="page-left">
+	
+	<div class="login-box clearfix">
+		
+		<form>
+			<fieldset>
+				<label for="user_email">Email Address:</label><br class="clear" />
+				<input type="text" id="user_email" name="user[email]" class="text-box" /><br class="clear" />
+				<label for="user_pass">Password:</label><br class="clear" />
+				<input type="text" id="user_pass" name="user[pass]" class="text-box" /><br class="clear" />
+				<input type="submit" id="submit_login" class="submit" value="Login">
+			</fieldset>
+		</form>
+		
+	</div>
+	
+	<div class="info-box clearfix">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus</p>
+	</div>
+	
+</div>
+
+<div class="page-right">
+	<h2>Page Title</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+	<h3>Page Sub-Title</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac elit a magna euismod venenatis. Duis in justo leo. Maecenas ligula purus, rhoncus at malesuada mattis, viverra ac libero. Vivamus dignissim libero quis ligula dignissim a semper dolor tristique. In hac habitasse platea dictumst. Aliquam consectetur lorem ut dui blandit a ultricies magna luctus. Aliquam eu metus purus, nec gravida ante. Curabitur vel nibh ipsum. Etiam quis sem quis arcu cursus condimentum. Aliquam et fringilla nulla.</p>
+</div>
+
+<br class="clear" />
 
 <?php $this->load->view('global/_new_footer.php'); ?>
