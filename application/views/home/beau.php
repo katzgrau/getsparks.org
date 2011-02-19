87fdbc13ad968786f7b788eac1b695897293058b
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-18 23:14:53 -0700

    More home page updates

--- application/views/home/beau.php
+++ application/views/home/beau.php
@@ -22,16 +22,22 @@
 			<?php foreach ($official_sparks as $spark) { ?>
 			<li class="clearfix">
 				<a style="font-size:16px;" href="<?php echo base_url(); ?>packages/<?php echo $spark->name; ?>/versions/HEAD/show"><img src="<?php echo GravatarHelper::from_email($spark->email, null, 40); ?>" /></a>
-				<p>
+				<p class="no-margin">
 					<a style="font-size:16px;" href="<?php echo base_url(); ?>packages/<?php echo $spark->name; ?>/versions/HEAD/show"><?php echo $spark->name; ?></a><br />
 					by: <a href="<?php echo base_url(); ?>contributors/<?php echo $spark->username; ?>/profile"><?php echo $spark->username; ?></a>
 				</p>
 				<br class="clear" />
-				<p><em><?php echo $spark->summary; ?></em></p>
+				<p class="no-margin"><em><?php echo $spark->summary; ?></em></p>
 			</li>
 			<?php } ?>
 			<li class="last"><a href="#">View All Official Sparks</a></li>
 		</ul>
+	</div>
+	
+	<div class="info-box clearfix">
+		<h2>Recent News</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod congue ligula quis pellentesque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris luctus consectetur lectus, in porttitor felis dictum ut. Pellentesque nec mi eget nisl sagittis ultricies. Quisque nunc nulla, facilisis nec sollicitudin vel, luctus nec elit. Vivamus imperdiet vehicula lacus, sit amet consectetur magna tristique facilisis.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod congue ligula quis pellentesque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris luctus consectetur lectus, in porttitor felis dictum ut. Pellentesque nec mi eget nisl sagittis ultricies. Quisque nunc nulla, facilisis nec sollicitudin vel, luctus nec elit. Vivamus imperdiet vehicula lacus, sit amet consectetur magna tristique facilisis.</p>
 	</div>
 	
 </div>
