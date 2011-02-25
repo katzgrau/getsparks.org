6f019ccee38d23bc9839e16c5526a0802426841c
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-25 00:52:35 -0700

    Updated with new skin

--- application/views/packages/edit.php
+++ application/views/packages/edit.php
@@ -1,74 +1,41 @@
 <?php
-    $this->load->view('global/_header.php');
+    $this->load->view('global/_new_header.php');
     $this->form_validation->set_error_delimiters('<li>', '</li>');
 ?>
 
-<p>
-    <a href="<?php echo base_url(); ?>contributors/<?php echo UserHelper::getUsername(); ?>/profile">
-        &larr; Back To Your Profile
-    </a>
-</p>
+<h2>Update Spark - <?=$contribution->name;?></h2>
+<p>Update away - try not to break anything though...we're getting low on sugar free Red Bull.</p>
 
 <ul>
     <?php echo validation_errors(); ?>
 </ul>
 
 <form action="<?php echo current_url(); ?>" method="post">
-    <div class="input-field">
-        <div class="field-name">
-            Name
-        </div>
-        <div class="field-value">
-            <input type="text" name="name" value="<?php echo set_value('name', $contribution->name); ?>" /><br />
-            <small>(only lowercase letters, dashes, underscores, and numbers)</small>
-        </div>
-    </div>
-    <div class="input-field">
-        <div class="field-name">
-            Summary
-        </div>
-        <div class="field-value">
-            <input type="text" name="summary" value="<?php echo set_value('summary', $contribution->summary); ?>" />
-        </div>
-    </div>
-    <div class="input-field">
-        <div class="field-name">
-            Description
-        </div>
-        <div class="field-value">
-            <textarea name="description"><?php echo set_value('description', $contribution->description); ?></textarea>
-        </div>
-    </div>
-    <div class="input-field">
-        <div class="field-name">
-            Repository Type
-        </div>
-        <div class="field-value">
-            <select name="repository_type">
-                <option value="hg" <?php if(set_value('repository_type', $contribution->repository_type) == 'hg') echo 'selected'; ?>>Mercurial (hg)</option>
-                <option value="git" <?php if(set_value('repository_type', $contribution->repository_type) == 'git') echo 'selected'; ?>>Git (git)</option>
-                <option value="zip" <?php if(set_value('repository_type', $contribution->repository_type) == 'zip') echo 'selected'; ?>>Zip (zip)</option>
-            </select>
-        </div>
-    </div>
-    <div class="input-field">
-        <div class="field-name">
-            Clone URL
-        </div>
-        <div class="field-value">
-            <input type="text" name="base_location" value="<?php echo set_value('base_location', $contribution->base_location); ?>" /><br />
-            <small>e.g., https://bitbucket.org/katzgrau/ci-sparks-repo</small>
-        </div>
-    </div>
-    <div class="input-field">
-        <div class="field-name">
-            Submit
-        </div>
-        <div class="field-value">
-            <input type="hidden" name="spark_id" value="<?php echo $contribution->id; ?>" />
-            <input type="submit" name="submit" value="Submit" />
-        </div>
-    </div>
+	
+	<label for="name">Package Name:</label><br class="clear" />
+	<input type="text" id="name" name="name" class="text-box" value="<?php echo set_value('name', $contribution->name); ?>" /><br class="clear" />
+	<span class="helper">Only lowercase letters, dashes, underscores, and numbers.</span><br class="clear" />
+	
+	<label for="summary">Summary:</label><br class="clear" />
+	<input type="text" id="summary" name="summary" class="text-box" value="<?php echo set_value('summary', $contribution->summary); ?>" /><br class="clear" />
+	<span class="helper">Keep it BRIEF...like ya drawers...</span><br class="clear" />
+	
+	<label for="description">Description:</label><br class="clear" />
+	<textarea id="description" name="description"><?php echo set_value('description', $contribution->description); ?></textarea><br class="clear" />
+	<span class="helper">You can get a little lengthy with this one...</span><br class="clear" />
+	
+	<label for="repository_type">Repository Type:</label><br class="clear" />
+	<select name="repository_type" id="repository_type" class="drop-down drop-down-small">
+		<option value="git" <?php if(set_value('repository_type', $contribution->repository_type) == 'git') echo 'selected'; ?>>Git (git)</option>
+		<option value="hg" <?php if(set_value('repository_type', $contribution->repository_type) == 'hg') echo 'selected'; ?>>Mercurial (hg)</option>
+	</select><br class="clear" />
+	
+	<label for="base_location">Public Clone URL:</label><br class="clear" />
+	<input type="text" id="base_location" name="base_location" class="text-box" value="<?php echo set_value('base_location', $contribution->base_location); ?>" /><br class="clear" />
+	<span class="helper">e.g., https://bitbucket.org/katzgrau/ci-sparks-repo</span><br class="clear" />
+	
+	<input type="submit" name="submit" value="Create Spark" class="button" />
+	
 </form>
 
-<?php $this->load->view('global/_footer.php'); ?>
+<?php $this->load->view('global/_new_header.php'); ?>
