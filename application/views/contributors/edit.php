feff26a9fd2ab7b4b1157783054859ef31a59468
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-25 00:35:18 -0700

    Updated form with helper text

--- application/views/contributors/edit.php
+++ application/views/contributors/edit.php
@@ -9,19 +9,29 @@ $this->form_validation->set_error_delimiters('<li>', '</li>');
     <?php echo validation_errors(); ?>
 </ul>
 
-<div class="form-wrapper">
+<div class="form-wrapper form-wrapper-full">
 	<form action="/contributors/<?=UserHelper::getUsername();?>/profile/edit" method="post">
 		<fieldset>
 			<label for="email">Email Address:</label><br class="clear" />
 			<input type="text" id="email" name="email" class="text-box" value="<?php echo set_value('email', $contributor->email); ?>" /><br class="clear" />
+			<span class="helper">We know you have a fancy-schmansy email address...right?</span><br class="clear" />
+			
 			<label for="real_name">Full Name:</label><br class="clear" />
 			<input type="text" id="real_name" name="real_name" class="text-box" value="<?php echo set_value('real_name', $contributor->real_name); ?>" /><br class="clear" />
+			<span class="helper">Please capitalize your First and Last name...John Doe!</span><br class="clear" />
+			
 			<label for="website">Web Site:</label><br class="clear" />
 			<input type="text" id="website" name="website" class="text-box" value="<?php echo set_value('website', $contributor->website); ?>" /><br class="clear" />
+			<span class="helper">For my sanity at this time - "http://www.YourSite.com"</span><br class="clear" />
+			
 			<label for="password">Password:</label><br class="clear" />
 			<input type="password" id="password" name="password" class="text-box" /><br class="clear" />
+			<span class="helper">Stop using your girlfriend's name as your password!</span><br class="clear" />
+			
 			<label for="password_again">Confirm Password:</label><br class="clear" />
 			<input type="password" id="password_again" name="password_again" class="text-box" /><br class="clear" />
+			<span class="helper">Seriously bro...</span><br class="clear" />
+			
 			<input type="submit" name="submit" value="Update Profile" class="button" />
 		</fieldset>
 	</form>
