3eb6410e450575bee597a4746355316a03cb498a
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-25 00:57:05 -0700

    Text tweaks

--- application/views/contributors/profile.php
+++ application/views/contributors/profile.php
@@ -2,7 +2,11 @@
 
 <h2>Profile for <?php echo $contributor->real_name; ?> (<?php echo $contributor->username; ?>)</h2>
 
-<table>
+<?php if($contributor->id == UserHelper::getId()): ?>
+	<p><a href="<?php echo base_url(); ?>contributors/<?php echo $contributor->username; ?>/profile/edit">Edit Your Profile</a></p>
+<?php endif; ?>
+
+<table style="margin-bottom:15px">
     <tr>
         <td>Username</td>
         <td>: <?php echo $contributor->username; ?></td>
@@ -17,29 +21,24 @@
     </tr>
 </table>
 
-<?php if($contributor->id == UserHelper::getId()): ?>
-<p>
-    <a href="<?php echo base_url(); ?>contributors/<?php echo $contributor->username; ?>/profile/edit">
-        Edit Your Profile
-    </a>
-</p>
-<?php endif; ?>
-
 <h3>Contributions</h3>
 <ul>
     <?php if(count($contributions) > 0): ?>
         <?php foreach($contributions as $contribution): ?>
             <li>
                 <a href="<?php echo base_url(); ?>packages/<?php echo $contribution->name; ?>/versions/HEAD/show">
-                    <?php echo $contribution->name; ?> <br />
-                    <small><?php echo $contribution->summary; ?></small>
+                    <?php echo $contribution->name; ?>
                 </a>
+				<br />
+                <small><?php echo $contribution->summary; ?></small>
             </li>
         <?php endforeach; ?>
     <?php else: ?>
             <li>You haven't contributed any sparks yet.</li>
     <?php endif; ?>
-	<li class="last" style="text-align:left"><a href="/packages/add" title="Create a Spark">Create a Spark</a></li>
+	<?php if ($contributor->id == UserHelper::getId()) { ?>
+		<li class="last" style="text-align:left"><a href="/packages/add" title="Create a Spark">Create a Spark</a></li>
+	<?php } ?>
 </ul>
 
 <?php $this->load->view('global/_new_footer.php'); ?>
