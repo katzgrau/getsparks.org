0ce9d697f8848a736ca78b60d5e198a4c01fa7d6
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-25 11:53:32 -0700

    Fixed update form

--- application/views/packages/edit.php
+++ application/views/packages/edit.php
@@ -10,32 +10,34 @@
     <?php echo validation_errors(); ?>
 </ul>
 
-<form action="<?php echo current_url(); ?>" method="post">
-	
-	<label for="name">Package Name:</label><br class="clear" />
-	<input type="text" id="name" name="name" class="text-box" value="<?php echo set_value('name', $contribution->name); ?>" /><br class="clear" />
-	<span class="helper">Only lowercase letters, dashes, underscores, and numbers.</span><br class="clear" />
-	
-	<label for="summary">Summary:</label><br class="clear" />
-	<input type="text" id="summary" name="summary" class="text-box" value="<?php echo set_value('summary', $contribution->summary); ?>" /><br class="clear" />
-	<span class="helper">Keep it BRIEF...like ya drawers...</span><br class="clear" />
-	
-	<label for="description">Description:</label><br class="clear" />
-	<textarea id="description" name="description"><?php echo set_value('description', $contribution->description); ?></textarea><br class="clear" />
-	<span class="helper">You can get a little lengthy with this one...</span><br class="clear" />
-	
-	<label for="repository_type">Repository Type:</label><br class="clear" />
-	<select name="repository_type" id="repository_type" class="drop-down drop-down-small">
-		<option value="git" <?php if(set_value('repository_type', $contribution->repository_type) == 'git') echo 'selected'; ?>>Git (git)</option>
-		<option value="hg" <?php if(set_value('repository_type', $contribution->repository_type) == 'hg') echo 'selected'; ?>>Mercurial (hg)</option>
-	</select><br class="clear" />
-	
-	<label for="base_location">Public Clone URL:</label><br class="clear" />
-	<input type="text" id="base_location" name="base_location" class="text-box" value="<?php echo set_value('base_location', $contribution->base_location); ?>" /><br class="clear" />
-	<span class="helper">e.g., https://bitbucket.org/katzgrau/ci-sparks-repo</span><br class="clear" />
-	
-	<input type="submit" name="submit" value="Update Spark" class="button" />
-	
-</form>
+<div class="form-wrapper form-wrapper-long">
+	<form action="<?php echo current_url(); ?>" method="post">
+
+		<label for="name">Package Name:</label><br class="clear" />
+		<input type="text" id="name" name="name" class="text-box" value="<?php echo set_value('name', $contribution->name); ?>" /><br class="clear" />
+		<span class="helper">Only lowercase letters, dashes, underscores, and numbers.</span><br class="clear" />
+
+		<label for="summary">Summary:</label><br class="clear" />
+		<input type="text" id="summary" name="summary" class="text-box" value="<?php echo set_value('summary', $contribution->summary); ?>" /><br class="clear" />
+		<span class="helper">Keep it BRIEF...like ya drawers...</span><br class="clear" />
+
+		<label for="description">Description:</label><br class="clear" />
+		<textarea id="description" name="description"><?php echo set_value('description', $contribution->description); ?></textarea><br class="clear" />
+		<span class="helper">You can get a little lengthy with this one...</span><br class="clear" />
+
+		<label for="repository_type">Repository Type:</label><br class="clear" />
+		<select name="repository_type" id="repository_type" class="drop-down drop-down-small">
+			<option value="git" <?php if(set_value('repository_type', $contribution->repository_type) == 'git') echo 'selected'; ?>>Git (git)</option>
+			<option value="hg" <?php if(set_value('repository_type', $contribution->repository_type) == 'hg') echo 'selected'; ?>>Mercurial (hg)</option>
+		</select><br class="clear" />
+
+		<label for="base_location">Public Clone URL:</label><br class="clear" />
+		<input type="text" id="base_location" name="base_location" class="text-box" value="<?php echo set_value('base_location', $contribution->base_location); ?>" /><br class="clear" />
+		<span class="helper">e.g., https://bitbucket.org/katzgrau/ci-sparks-repo</span><br class="clear" />
+
+		<input type="submit" name="submit" value="Update Spark" class="button" />
+
+	</form>
+</div>
 
 <?php $this->load->view('global/_new_footer.php'); ?>
