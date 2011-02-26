a8b2962a249ed6ccecf85bdfd641d767bb84bf49
Kenny Katzgrau <katzgrau@gmail.com>
2011-02-26 14:20:46 -0500

    Updated sparks package url

--- application/config/application.php
+++ application/config/application.php
@@ -8,7 +8,7 @@ $config['sparks_download_button_text'] = 'Get Sparks Now!';
 
 $config['sparks_download_button_url'] = config_item('base_url').'set-up';
 
-$config['sparks_download_url'] = config_item('base_url').'static/install/spark-manager-0.0.2.zip';
+$config['sparks_download_url'] = config_item('base_url').'static/install/spark-manager-0.0.3.zip';
 
 $config['sparks_loader_url'] = config_item('base_url').'static/install/MY_Loader.php.txt';
 
