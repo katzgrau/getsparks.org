985c8591def6426a0fcc13e6e4eda1995c871827
Beau Frusetta <beau.frusetta@gmail.com>
2011-02-28 09:17:23 -0700

    Added lazy loading of profile in left column via ajax call

--- application/controllers/contributors.php
+++ application/controllers/contributors.php
@@ -137,4 +137,23 @@ class Contributors extends CI_Controller
         $this->form_validation->set_message('robot_check', 'The robot check was wrong. hrmm.');
         return FALSE;
     }
+
+	public function get_profile_info()
+	{
+		$html  = '<div class="profile-image">';
+		$html .= '<a href="/contributors/profile/'.UserHelper::getUsername().'" title="View Your Profile">';
+		$html .= '<img src="'.UserHelper::getAvatarURL(80).'" alt="Gravatar" />';
+		$html .= '</a>';
+		$html .= '</div>';
+		$html .= '<div class="profile-links">';
+		$html .= '<dl>';
+		$html .= '<dd><a href="/contributors/profile/'.UserHelper::getUsername().'" title="View Your Profile">View Your Profile</a></dd>';
+		$html .= '<dd><a href="/contributors/'.UserHelper::getUsername().'/profile/edit" title="Update Your Profile">Update Your Profile</a></dd>';
+		$html .= '<dd><a href="/packages/add" title="Create a Spark">Create a Spark</a></dd>';
+		$html .= '<dd class="last"><a href="/logout" title="Logout">Logout</a></dd>';
+		$html .= '</dl>';
+		$html .= '</div>';
+		
+		echo $html;
+	}
 }
--- application/views/global/_left_column.php
+++ application/views/global/_left_column.php
@@ -1,3 +1,9 @@
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+		$("#lazy_profile").load("/contributors/get_profile_info");
+	});
+</script>
+
 <?php
 
 $this->load->model('spark');
@@ -21,19 +27,7 @@ $official_sparks = Spark::getLatestOf(3, NULL, TRUE);
 				</fieldset>
 			</form>
 		<?php else: ?>
-			<div class="profile-image">
-				<a href="/contributors/profile/<?php echo UserHelper::getUsername(); ?>" title="View Your Profile">
-					<img src="<?php echo UserHelper::getAvatarURL(80); ?>" alt="Gravatar" />
-				</a>
-			</div>
-			<div class="profile-links">
-				<dl>
-					<dd><a href="/contributors/profile/<?php echo UserHelper::getUsername(); ?>" title="View Your Profile">View Your Profile</a></dd>
-					<dd><a href="/contributors/<?php echo UserHelper::getUsername(); ?>/profile/edit" title="Update Your Profile">Update Your Profile</a></dd>
-					<dd><a href="/packages/add" title="Create a Spark">Create a Spark</a></dd>
-					<dd class="last"><a href="/logout" title="Logout">Logout</a></dd>
-				</dl>
-			</div>
+			<div id="lazy_profile"></div>
 		<?php endif; ?>
 		
 	</div>
