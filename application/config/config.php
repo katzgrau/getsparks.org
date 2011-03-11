dd6acb4676e5131a174500e50e2fcaeaa93f9b0e
Kenny Katzgrau <katzgrau@gmail.com>
2011-03-10 21:42:46 -0500

    Removing default config items

--- application/config/config.php
+++ application/config/config.php
@@ -16,8 +16,7 @@ $config['is_production'] = FALSE;
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://sparks.codeigniter.com/';
-# $config['base_url']	= 'http://localhost:8888/';
+$config['base_url']	= '';
 /*
 |--------------------------------------------------------------------------
 | Index File
