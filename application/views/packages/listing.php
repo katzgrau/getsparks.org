dcd1741ffc9f2ae050a232c53025e663e8e6a16f
unknown <Sean@.(none)>
2011-03-12 20:54:18 +0000

    Changes to the layout of the browse pages

--- application/views/packages/listing.php
+++ application/views/packages/listing.php
@@ -5,15 +5,28 @@ $this->load->view('global/_new_header.php', array('title' => $title)); ?>
 <h2><?php echo $title; ?></h2>
 
 <p><?php echo $description; ?></p>
-<ul>
+<ul id="sparklisting">
     <?php if(count($sparks) > 0): ?>
         <?php foreach($sparks as $spark): ?>
             <li>
-                <a href="<?php echo base_url(); ?>packages/<?php echo $spark->name; ?>/versions/HEAD/show">
-                    <?php echo $spark->name; ?>
-                </a>
-				<br />
-                <small><?php echo $spark->summary; ?></small>
+				<div class="sparkinfo">
+					<a href="<?php echo base_url(); ?>packages/<?php echo $spark->name; ?>/versions/HEAD/show">
+						<?php echo $spark->name; ?>
+					</a> by
+					<a href="<?php echo base_url(); ?>contributors/<?php echo $spark->username; ?>/profile">
+						<?php echo $spark->username; ?>
+					</a>
+					<span><?php echo $spark->summary; ?></span>
+				</div>
+				<div class="sparkstats">
+					<ul>
+						<li><span>Number of Installs: </span> <?php echo number_format($spark->getInstallCount()); ?></li>
+						<li><span>Official: </span> <?php echo $spark->is_official == 1 ? 'Yes' : 'No'; ?></li>
+						<li><span>Featured: </span> <?php echo $spark->is_featured == 1 ? 'Yes' : 'No'; ?></li>
+						<li><span>Supported: </span> <?php echo $spark->is_unsupported == 0 ? 'Yes' : 'No'; ?></li>
+						<li><span>Approved: </span> <?php echo $spark->is_approved == 1 ? 'Yes' : 'No'; ?></li>
+					</ul>
+				</div>
             </li>
         <?php endforeach; ?>
     <?php else: ?>
