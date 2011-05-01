8251d4ce9d26b528f284e3b1ffdeef4202bab3ff
John Crepezzi <john.crepezzi@gmail.com>
2011-04-30 22:38:56 -0400

    Prevent default click action

--- static/scripts/site.js
+++ static/scripts/site.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
-	$('#package_ratings .ratings .rating').click(function() {
+	$('#package_ratings .ratings .rating').click(function(e) {
 
 		$('#package_ratings .ratings .rating.selected').removeClass('selected');
 		$(this).addClass('selected');
+
+		e.preventDefault();
 
 	});
 
