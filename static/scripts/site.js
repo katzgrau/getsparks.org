799e8be88fc9be08600b010aa7b918407f141c83
John Crepezzi <john.crepezzi@gmail.com>
2011-04-30 22:47:42 -0400

    Clean up js selector

--- static/scripts/site.js
+++ static/scripts/site.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-	$('#package_ratings a.ratings .rating').click(function(e) {
+	$('#package_ratings a.rating').click(function(e) {
 
-		$('#package_ratings a.ratings .rating.selected').removeClass('selected');
+		$('#package_ratings a.rating.selected').removeClass('selected');
 		$(this).addClass('selected');
 
 		e.preventDefault();
